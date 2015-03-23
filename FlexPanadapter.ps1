a41ff1f820cecf0ec5321f2eba1758ea1355d71c
Robbie Foust <rfoust@gmail.com>
2015-03-22 22:45:29 -0400

    placeholder for future new-flexpanadapter

--- FlexPanadapter.ps1
+++ FlexPanadapter.ps1
@@ -71,6 +71,63 @@ function get-FlexPanadapter
     end { }
     }
 
+<#
+function new-FlexPanadapter
+    {
+    [CmdletBinding(DefaultParameterSetName="p0")]
+    param(
+        [Parameter(ParameterSetName="p0",Position=0, ValueFromPipelineByPropertyName = $true)]
+        [string]$Serial
+        )
+
+    begin { }
+
+    process 
+        {
+        if (-not $Serial)
+            {
+            $radios = get-FlexRadio
+
+            if ($radios.count -eq 1)
+                {
+                write-verbose "One FlexRadio found. Using it."
+                $Serial = $radios[0].serial
+                }
+            else
+                {
+                throw "Specify radio to use by serial number with -Serial argument, or use pipeline."
+                }
+            }
+
+        foreach ($radio in $Serial)
+            {
+            $radioObj = get-FlexRadio -Serial:$radio
+
+            write-verbose "Serial: $($radioObj.serial)"
+
+            if (-not $radioObj.serial)
+                {
+                continue
+                }
+            
+            write-verbose "Radio connected: $($radioObj.connected)"
+
+            if ($radioObj.Connected -eq $false)
+                {
+                throw "Not connected to $($radioObj.model): $($radioObj.serial). Use connect-flexradio to establish a new connection."
+                }
+
+            $panadapters = $null
+
+            # todo: create new panadapter - this won't work anyway right now, stream data will get sent here instead of to smartsdr.
+            }
+        }
+
+    end { }
+    }
+#>
+
+
 function remove-FlexPanadapter
     {
     [CmdletBinding(DefaultParameterSetName="p0",
