b11fe7993a18607d31a004038c92e96ed35b9bf4
Robbie Foust <rfoust@gmail.com>
2015-03-22 22:24:15 -0400

    added remove-FlexSliceReceiver

--- FlexSlice.ps1
+++ FlexSlice.ps1
@@ -89,6 +89,74 @@ function get-FlexSliceReceiver
     end { }
     }
 
+function remove-FlexSliceReceiver
+    {
+    [CmdletBinding(DefaultParameterSetName="p0",
+        SupportsShouldProcess=$true,
+        ConfirmImpact="High")]
+    param(
+        [Parameter(ParameterSetName="p0",Position=0, ValueFromPipelineByPropertyName = $true)]
+        [string]$Serial,
+
+        [Parameter(ParameterSetName="p0",Position=1, ValueFromPipelineByPropertyName = $true)]
+        [int]$Index
+        )
+
+    begin { }
+
+    process 
+        {
+        if (-not $Serial)
+            {
+            $radios = get-FlexRadio
+
+            if ($radios.count -eq 1)
+                {
+                write-verbose "One FlexRadio found. Using it."
+                $Serial = $radios[0].serial
+                }
+            else
+                {
+                throw "Specify radio to use by serial number with -Serial argument, or use pipeline."
+                }
+            }
+
+        foreach ($radio in $Serial)
+            {
+            $radioObj = get-FlexRadio -Serial:$radio
+
+            write-verbose "Serial: $($radioObj.serial)"
+
+            if (-not $radioObj.serial)
+                {
+                continue
+                }
+            
+            write-verbose "Radio connected: $($radioObj.connected)"
+
+            if ($radioObj.Connected -eq $false)
+                {
+                throw "Not connected to $($radioObj.model): $($radioObj.serial). Use connect-flexradio to establish a new connection."
+                }
+
+            $slices = get-FlexSliceReceiver -Index:$Index
+
+            foreach ($slice in $slices)
+                {
+                if ($slice.Index)
+                    {
+                    if ($pscmdlet.ShouldProcess($radioObj.Serial,"Delete Slice #$($slice.Index)"))
+                        {
+                        $slice.remove($true)
+                        }
+                    }
+                }
+            }
+        }
+
+    end { }
+    }
+
 function set-FlexSliceReceiver
     {
     [CmdletBinding(DefaultParameterSetName="p0",
